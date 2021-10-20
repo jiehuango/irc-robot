86e584d61c48afa1fa2c0d55141abf206ccebb11
jiehuango <3356136312@qq.com>
2021-10-20 20:10:10 +0800

    The ping timeout error fixed.

--- robot.py
+++ robot.py
@@ -23,13 +23,22 @@ irc_sock.send(("JOIN " + irc_chan + "\r\n").encode())
 while True:
 	data = irc_sock.makefile(encoding='utf-8')
 	for line in data:
-		if(("libera.chat" in line)==False):
+		if (("libera.chat" in line) and ("ChanServ" in line) and ("services" in line)):
+			
+			print(line.encode('utf-8').decode())
+			
+		elif ("hello" in line):
 			print(line.encode('utf-8').decode())
 			hello = bytes("hello","utf-8")
 			a = "PRIVMSG " + "##huan" + " :"  + "hello\r\n"
 			a = a.encode("utf-8")
 			irc_sock.send(a)
-			
-			
+			print("[robot] hello")
+		elif ("Ping timeout" in line):
+			irc_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			irc_sock.connect((irc_host, irc_port))
+			irc_sock.send(("NICK " + bot_name + "\r\n").encode())
+			irc_sock.send(("USER " + bot_name + " " + bot_name + " " + usr_name + " :" + ral_name + "\r\n").encode())
+			irc_sock.send(("JOIN " + irc_chan + "\r\n").encode())
 		else:
 			print(line.encode('utf-8').decode())
